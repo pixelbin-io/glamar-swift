7e23db05dd8d9b075fea663a68b2b99017b854bb
Kushal Parmar <kushalparmar@gofynd.com>
2026-07-16 22:35:39 +0530

    config changes

--- Sources/GlamAR/GlamAR.swift
+++ Sources/GlamAR/GlamAR.swift
@@ -107,11 +107,27 @@ public class GlamAr {
         ])
     }
 
-    public static func configChange(type: String, value: Double) {
-        postMessage(type: "onConfigChange", payload: [
-            "type": type,
-            "value": value
-        ])
+    public static func configChange(
+        type: String,
+        value: Double? = nil,
+        skuId: String? = nil,
+        subCategory: String? = nil
+    ) {
+        var payload: [String: Any] = ["type": type]
+
+        if let value {
+            payload["value"] = value
+        }
+
+        if let skuId {
+            payload["skuId"] = skuId
+        }
+
+        if let subCategory {
+            payload["subCategory"] = subCategory
+        }
+
+        postMessage(type: "onConfigChange", payload: payload)
     }
 
     public static func onNailColorEvents(options: String? = nil, value: Any? = nil) {
